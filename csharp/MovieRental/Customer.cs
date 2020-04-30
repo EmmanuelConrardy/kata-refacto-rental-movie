0495c496064aeb2fdc3317dad71364bd652a5713
econrardy <emmanuel.conrardy@arolla.fr>
2020-04-30 23:45:24 +0200

    Extract method

--- csharp/MovieRental/Customer.cs
+++ csharp/MovieRental/Customer.cs
@@ -33,30 +33,12 @@ namespace MovieRental
             foreach (Rental each in _rentals)
             {
                 double thisAmount = 0;
+                
+                //Extract method
+                thisAmount = GetAmount(each, thisAmount);
 
-                //determine amounts for each line
-                switch (each.getMovie().getPriceCode())
-                {
-                    case Movie.REGULAR:
-                        thisAmount += 2;
-                        if (each.getDaysRented() > 2)
-                            thisAmount += (each.getDaysRented() - 2) * 1.5;
-                        break;
-                    case Movie.NEW_RELEASE:
-                        thisAmount += each.getDaysRented() * 3;
-                        break;
-                    case Movie.CHILDRENS:
-                        thisAmount += 1.5;
-                        if (each.getDaysRented() > 3)
-                            thisAmount += (each.getDaysRented() - 3) * 1.5;
-                        break;
-                }
-
-                // add frequent renter points
-                frequentRenterPoints++;
-                // add bonus for a two day new release rental
-                if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-                    frequentRenterPoints++;
+                //Extract method
+                frequentRenterPoints = GetFrequentRenterPoints(frequentRenterPoints, each);
 
                 // show figures for this rental
                 result += "\t" + each.getMovie().getTitle() + "\t" + thisAmount.ToString() + "\n";
@@ -69,6 +51,39 @@ namespace MovieRental
             result += "You earned " + frequentRenterPoints.ToString() + " frequent renter points";
 
             return result;
+        }
+
+        private double GetAmount(Rental each, double thisAmount)
+        {
+            //determine amounts for each line
+            switch (each.getMovie().getPriceCode())
+            {
+                case Movie.REGULAR:
+                    thisAmount += 2;
+                    if (each.getDaysRented() > 2)
+                        thisAmount += (each.getDaysRented() - 2) * 1.5;
+                    break;
+                case Movie.NEW_RELEASE:
+                    thisAmount += each.getDaysRented() * 3;
+                    break;
+                case Movie.CHILDRENS:
+                    thisAmount += 1.5;
+                    if (each.getDaysRented() > 3)
+                        thisAmount += (each.getDaysRented() - 3) * 1.5;
+                    break;
+            }
+
+            return thisAmount;
+        }
+
+        private int GetFrequentRenterPoints(int frequentRenterPoints, Rental each)
+        {
+            // add frequent renter points
+            frequentRenterPoints++;
+            // add bonus for a two day new release rental
+            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
+                frequentRenterPoints++;
+            return frequentRenterPoints;
         }
     }
 
