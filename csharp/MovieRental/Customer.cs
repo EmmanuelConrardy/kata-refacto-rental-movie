19fdf222107d081f064cf8e235ae5215427174f5
econrardy <emmanuel.conrardy@arolla.fr>
2020-05-01 00:11:20 +0200

    Cleaning

--- csharp/MovieRental/Customer.cs
+++ csharp/MovieRental/Customer.cs
@@ -6,8 +6,6 @@ namespace MovieRental
     {
 
         private string _name;
-        //Missing abstraction
-        private List<Rental> _rentals = new List<Rental>();
         private Rentals _rentals_ = new Rentals();
 
 
@@ -18,7 +16,6 @@ namespace MovieRental
 
         public void addRental(Rental arg)
         {
-            _rentals.Add(arg);
             _rentals_.Add(arg);
         }
 
@@ -29,31 +26,11 @@ namespace MovieRental
 
         public string statement()
         {
-           
             string result = "Rental Record for " + getName() + "\n";
-
-            //START - Test seams
-            //double totalAmount = 0;
-            //int frequentRenterPoints = 0;
-            //foreach (Rental each in _rentals)
-            //{
-            //    totalAmount += each.GetAmount();
-            //    frequentRenterPoints += each.GetFrequentRenterPoints();
-            //    result += each.ToString();
-            //}
-            //END - Test seams
             result += _rentals_.ToString();
-           // add footer lines
-           result += "Amount owed is " + _rentals_.GetTotalAmount().ToString() + "\n";
+            result += "Amount owed is " + _rentals_.GetTotalAmount().ToString() + "\n";
             result += "You earned " + _rentals_.GetfrequentRenterPoints().ToString() + " frequent renter points";
 
-            return result;
-        }
-
-        private static string PrintRental(string result, Rental each, double thisAmount)
-        {
-            // show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" + thisAmount.ToString() + "\n";
             return result;
         }
     }
