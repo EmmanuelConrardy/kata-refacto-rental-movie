fcdaef8132634e41dd425394ffb92186088470a9
econrardy <emmanuel.conrardy@arolla.fr>
2020-04-30 22:42:05 +0200

    Add Application Console

---
+++ csharp/MovieRental.Front/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MovieRental.Front
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie movie = new Movie("Transformer", Movie.REGULAR);
+
+            Rental rental = new Rental(movie, 3);
+
+            Customer customer = new Customer("Arolla");
+            customer.addRental(rental);
+
+            String statement = customer.statement();
+            Console.WriteLine(statement);
+            Console.ReadKey();
+        }
+    }
+}
