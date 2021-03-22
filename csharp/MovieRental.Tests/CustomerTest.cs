c691bd27a5c88413b84d8bb637059554a1246959
emmanuel.conrardy <emmanuel.conrardy@acadomia.fr>
2021-03-22 22:49:17 +0100

    refact test : use var

--- csharp/MovieRental.Tests/CustomerTest.cs
+++ csharp/MovieRental.Tests/CustomerTest.cs
@@ -8,100 +8,100 @@ namespace MovieRental.Tests
         [TestMethod]
         public void TestCustomer()
         {
-            Customer c = new CustomerBuilder().build();
+            var c = new CustomerBuilder().build();
             Assert.IsNotNull(c);
         }
 
         [TestMethod]
         public void TestAddRental()
         {
-            Customer customer2 = new CustomerBuilder().withName("Julia").build();
-            Movie movie1 = new Movie("Gone with the Wind", Movie.REGULAR);
-            Rental rental1 = new Rental(movie1, 3); // 3 day rental
+            var customer2 = new CustomerBuilder().withName("Julia").build();
+            var movie1 = new Movie("Gone with the Wind", Movie.REGULAR);
+            var rental1 = new Rental(movie1, 3); // 3 day rental
             customer2.addRental(rental1);
         }
 
         [TestMethod]
         public void TestGetName()
         {
-            Customer c = new Customer("David");
+            var c = new Customer("David");
             Assert.AreEqual("David", c.getName());
         }
         [TestMethod]
         public void StatementForRegularMovie()
         {
-            Movie movie1 = new Movie("Gone with the Wind", Movie.REGULAR);
-            Rental rental1 = new Rental(movie1, 3); // 3 day rental
-            Customer customer2 =
+            var movie1 = new Movie("Gone with the Wind", Movie.REGULAR);
+            var rental1 = new Rental(movie1, 3); // 3 day rental
+            var customer2 =
                     new CustomerBuilder()
                             .withName("Sallie")
                             .withRentals(rental1)
                             .build();
-            string expected = "Rental Record for Sallie\n" +
-                    "\tGone with the Wind\t3,5\n" +
-                    "Amount owed is 3,5\n" +
-                    "You earned 1 frequent renter points";
-            string statement = customer2.statement();
+            var expected = "Rental Record for Sallie\n" +
+                           "\tGone with the Wind\t3,5\n" +
+                           "Amount owed is 3,5\n" +
+                           "You earned 1 frequent renter points";
+            var statement = customer2.statement();
             Assert.AreEqual(expected, statement);
         }
 
         [TestMethod]
         public void StatementForNewReleaseMovie()
         {
-            Movie movie1 = new Movie("Star Wars", Movie.NEW_RELEASE);
-            Rental rental1 = new Rental(movie1, 3); // 3 day rental
-            Customer customer2 =
+            var movie1 = new Movie("Star Wars", Movie.NEW_RELEASE);
+            var rental1 = new Rental(movie1, 3); // 3 day rental
+            var customer2 =
                     new CustomerBuilder()
                             .withName("Sallie")
                             .withRentals(rental1)
                             .build();
-            string expected = "Rental Record for Sallie\n" +
-                    "\tStar Wars\t9\n" +
-                    "Amount owed is 9\n" +
-                    "You earned 2 frequent renter points";
-            string statement = customer2.statement();
+            var expected = "Rental Record for Sallie\n" +
+                           "\tStar Wars\t9\n" +
+                           "Amount owed is 9\n" +
+                           "You earned 2 frequent renter points";
+            var statement = customer2.statement();
             Assert.AreEqual(expected, statement);
         }
 
         [TestMethod]
         public void StatementForChildrensMovie()
         {
-            Movie movie1 = new Movie("Madagascar", Movie.CHILDRENS);
-            Rental rental1 = new Rental(movie1, 3); // 3 day rental
-            Customer customer2
+            var movie1 = new Movie("Madagascar", Movie.CHILDRENS);
+            var rental1 = new Rental(movie1, 3); // 3 day rental
+            var customer2
                     = new CustomerBuilder()
                     .withName("Sallie")
                     .withRentals(rental1)
                     .build();
-            string expected = "Rental Record for Sallie\n" +
-                    "\tMadagascar\t1,5\n" +
-                    "Amount owed is 1,5\n" +
-                    "You earned 1 frequent renter points";
-            string statement = customer2.statement();
+            var expected = "Rental Record for Sallie\n" +
+                           "\tMadagascar\t1,5\n" +
+                           "Amount owed is 1,5\n" +
+                           "You earned 1 frequent renter points";
+            var statement = customer2.statement();
             Assert.AreEqual(expected, statement);
         }
 
         [TestMethod]
         public void StatementForManyMovies()
         {
-            Movie movie1 = new Movie("Madagascar", Movie.CHILDRENS);
-            Rental rental1 = new Rental(movie1, 6); // 6 day rental
-            Movie movie2 = new Movie("Star Wars", Movie.NEW_RELEASE);
-            Rental rental2 = new Rental(movie2, 2); // 2 day rental
-            Movie movie3 = new Movie("Gone with the Wind", Movie.REGULAR);
-            Rental rental3 = new Rental(movie3, 8); // 8 day rental
-            Customer customer1
+            var movie1 = new Movie("Madagascar", Movie.CHILDRENS);
+            var rental1 = new Rental(movie1, 6); // 6 day rental
+            var movie2 = new Movie("Star Wars", Movie.NEW_RELEASE);
+            var rental2 = new Rental(movie2, 2); // 2 day rental
+            var movie3 = new Movie("Gone with the Wind", Movie.REGULAR);
+            var rental3 = new Rental(movie3, 8); // 8 day rental
+            var customer1
                     = new CustomerBuilder()
                     .withName("David")
                     .withRentals(rental1, rental2, rental3)
                     .build();
-            string expected = "Rental Record for David\n" +
-                    "\tMadagascar\t6\n" +
-                    "\tStar Wars\t6\n" +
-                    "\tGone with the Wind\t11\n" +
-                    "Amount owed is 23\n" +
-                    "You earned 4 frequent renter points";
-            string statement = customer1.statement();
+            var expected = "Rental Record for David\n" +
+                           "\tMadagascar\t6\n" +
+                           "\tStar Wars\t6\n" +
+                           "\tGone with the Wind\t11\n" +
+                           "Amount owed is 23\n" +
+                           "You earned 4 frequent renter points";
+            var statement = customer1.statement();
             Assert.AreEqual(expected, statement);
         }
 
@@ -111,21 +111,21 @@ namespace MovieRental.Tests
         {
             //Arrange
             var regularMovie = new Movie("Gone with the Wind", Movie.REGULAR);
-            Rental rental = new Rental(regularMovie, 12); 
-            Customer Sallie =
+            var rental = new Rental(regularMovie, 12); 
+            var Sallie =
                 new CustomerBuilder()
                     .withName("Sallie")
                     .withRentals(rental)
                     .build();
             
             //Act
-            string statement = Sallie.statement();
+            var statement = Sallie.statement();
 
             //Assert
-            string expected = "Rental Record for Sallie\n" +
-                              "\tGone with the Wind\t16\n" +
-                              "Amount owed is 16\n" +
-                              "You earned 1 frequent renter points";
+            var expected = "Rental Record for Sallie\n" +
+                           "\tGone with the Wind\t16\n" +
+                           "Amount owed is 16\n" +
+                           "You earned 1 frequent renter points";
             Assert.AreEqual(expected, statement);
         }
     }
