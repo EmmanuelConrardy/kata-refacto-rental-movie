0b8fcf7060a49c02c51781333fd2c7261b904c3d
emmanuel.conrardy <emmanuel.conrardy@acadomia.fr>
2021-03-22 23:27:41 +0100

    refact Customer : Rename amount and rental

--- csharp/MovieRental/Customer.cs
+++ csharp/MovieRental/Customer.cs
@@ -30,41 +30,41 @@ namespace MovieRental
             int frequentRenterPoints = 0;
             string result = "Rental Record for " + getName() + "\n";
 
-            foreach (Rental each in _rentals)
+            foreach (Rental rental in _rentals)
             {
-                double thisAmount = 0;
+                double amount = 0;
 
                 //determine amounts for each line
                 //Code smell : Message Chains
                 //Code smell : Switch Statements
-                switch (each.getMovie().getPriceCode())
+                switch (rental.getMovie().getPriceCode())
                 {
                     case Movie.REGULAR:
-                        thisAmount += 2;
-                        if (each.getDaysRented() > 2)
-                            thisAmount += (each.getDaysRented() - 2) * 1.5;
-                        if (each.getDaysRented() > 10)
-                            thisAmount -= (each.getDaysRented() - 10) * 0.5;
+                        amount += 2;
+                        if (rental.getDaysRented() > 2)
+                            amount += (rental.getDaysRented() - 2) * 1.5;
+                        if (rental.getDaysRented() > 10)
+                            amount -= (rental.getDaysRented() - 10) * 0.5;
                         break;
                     case Movie.NEW_RELEASE:
-                        thisAmount += each.getDaysRented() * 3;
+                        amount += rental.getDaysRented() * 3;
                         break;
                     case Movie.CHILDRENS:
-                        thisAmount += 1.5;
-                        if (each.getDaysRented() > 3)
-                            thisAmount += (each.getDaysRented() - 3) * 1.5;
+                        amount += 1.5;
+                        if (rental.getDaysRented() > 3)
+                            amount += (rental.getDaysRented() - 3) * 1.5;
                         break;
                 }
 
                 // add frequent renter points
                 frequentRenterPoints++;
                 // add bonus for a two day new release rental
-                if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
+                if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && rental.getDaysRented() > 1)
                     frequentRenterPoints++;
 
                 // show figures for this rental
-                result += "\t" + each.getMovie().getTitle() + "\t" + thisAmount.ToString() + "\n";
-                totalAmount += thisAmount;
+                result += "\t" + rental.getMovie().getTitle() + "\t" + amount.ToString() + "\n";
+                totalAmount += amount;
             }
 
             // add footer lines
