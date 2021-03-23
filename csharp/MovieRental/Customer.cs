d00ffff4c53afb289d0741e1978905d3b4d219b0
emmanuel.conrardy <emmanuel.conrardy@acadomia.fr>
2021-03-23 16:32:43 +0100

    refact customer : Remove dead code Customer

--- csharp/MovieRental/Customer.cs
+++ csharp/MovieRental/Customer.cs
@@ -6,17 +6,11 @@ namespace MovieRental
     {
 
         private string _name;
-        private List<Rental> _rentals = new List<Rental>();
         private List<RentalBase> _rentalsBase = new List<RentalBase>();
 
         public Customer(string name)
         {
             _name = name;
-        }
-
-        public void addRental(Rental arg)
-        {
-            _rentals.Add(arg);
         }
 
         public void addRental(RentalBase arg)
@@ -34,15 +28,6 @@ namespace MovieRental
             double totalAmount = 0;
             string result = "Rental Record for " + getName() + "\n";
             var frequentRenterPoints = 0;
-
-            foreach (Rental rental in _rentals)
-            {
-                result += "\t" + rental.getTitle() + "\t" + rental.GetAmount() + "\n";
-               
-                frequentRenterPoints += rental.FrequentRenterPoints();
-                
-                totalAmount += rental.GetAmount();
-            }
 
             foreach (var rental in _rentalsBase)
             {
